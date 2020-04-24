458a20240e6adb60565c323e12d017d2c7a4dd39
GGVidal <gabrielvidalgondim@gmail.com>
2020-04-24 19:02:06 -0300

    Listing providers and loading images

--- src/app/controllers/FileController.js
+++ src/app/controllers/FileController.js
@@ -1,6 +1,18 @@
+import File from '../models/File';
+
 class FileController {
     async store(req, res) {
-        return res.json(req.file);
+        try {
+            const { originalname: name, filename: path } = req.file;
+
+            const file = await File.create({
+                name,
+                path,
+            });
+            return res.json(file);
+        } catch (error) {
+            console.log(error);
+        }
     }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -6,13 +6,13 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import Session from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
-
+import ProviderController from './app/controllers/ProviderController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 const upload = multer(multerConfig);
 
-routes.use(cors());
+// routes.use(cors());
 routes.post('/users', UserController.store);
 routes.post('/sessions', Session.store);
 
@@ -20,5 +20,7 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
-// routes.post('/files', upload.single('file'), FileController.store());
+routes.post('/files', upload.single('file'), FileController.store);
+
+routes.get('/providers', ProviderController.index);
 export default routes;
