c09aa96fae5b904482fbe942d19e9ebc16e985fe
GGVidal <gabrielvidalgondim@gmail.com>
2020-04-27 22:46:02 -0300

    Sending e-mail

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    secure: false,
+    auth: {
+        user: '395da980b36fde',
+        pass: 'fcd2e4b4f9c1bb',
+    },
+    default: {
+        from: 'Gabriel Gondim <noreply@gobarber.com>',
+    },
+};
